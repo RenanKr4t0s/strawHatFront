3232f68bc0d0dc8dc6f354b3d4cc24b955209ef7
RenanKr4t0s <redrums88@gmail.com>
2024-12-16 17:42:51 -0300

    start da inteligancia do Card Game, p1 e p2 interagindo

--- src/CardGame.tsx
+++ src/CardGame.tsx
@@ -4,24 +4,50 @@ import CardForP2 from "./components/CardForP2"
 
 
 import playables from "./playables"
+import { useState } from "react"
 
 export default function CardGame() {
+    const [p1Turno, setP1Turno] = useState(true)
+    const [player1, setPlayer1] = useState(playables["zoro"])
+    const [player2, setPlayer2] = useState(playables["enel"])
+    function toggleTurno(){
+        setP1Turno(!p1Turno)
+    }
   return (
-    <div className="bg-red-300 h-screen flex justify-center">
+    <div className="h-screen flex justify-center">
         <div className="p-6 text-center space-y-8 bg-slate-300 w-full md:w-3/4 max-w-4xl m-8">
             <h1 className="text-3xl font-bold mb-4 text-orange-500">Straw Hat Card Game</h1>
             <div className="flex gap-3">
                 <div className="w-96 bg-slate-400">
-                    <CardForP1 cardCharacter={playables["zoro"]} />
+                    <CardForP1 
+                        stateCharacter={player1}
+                        setStateCharacter={setPlayer1}
+                        stateEnemy={player2}
+                        setStateEnemy={setPlayer2}
+                        p1Turno={p1Turno}
+                        toggleTurno={toggleTurno}
+                    />
                 </div>
-                <div className="w-16 bg-red-200 flex items-center">
+                <div className="w-16 flex items-center">
                     <h2>Player vs Player</h2>
                 </div>
                 <div className="w-96 bg-slate-400">
-                    <CardForP2 cardCharacter={playables["zoro"]} />
+                    <CardForP1 
+                        stateCharacter={player2}
+                        setStateCharacter={setPlayer2}
+                        stateEnemy={player1}
+                        setStateEnemy={setPlayer1}
+                        p1Turno={p1Turno}
+                        toggleTurno={toggleTurno}
+                    />
                 </div>
             </div>
-            <p className="text-gray-600">Aqui nós vamos construir toda a aplicação de Game</p>
+
+
+
+            {/* Criar aqui o "Escolha os personagens" com um Button para o P1 e outro para o P2 escolher entre as possibilidades */}
+
+
 
             <Link to={"/"} className=" inline-block text-zinc-200 bg-yellow-600 rounded-lg py-2 px-4 hover:bg-yellow-800 hover:text-zinc-50 transition-colors"  >Voltar para Home
             </Link>
--- src/components/CardForP1.tsx
+++ src/components/CardForP1.tsx
@@ -1,41 +1,79 @@
 // Esse elemento simplesmente recebe um card e mostra ele na tela
 // Possíveis animações aqui
 
-import image from "../assets/luffy512.png"
+import { useState } from "react";
 import {Character} from "../playables"
 
 interface CardForGameProps {
-    cardCharacter: Character;
+    stateCharacter: Character;
+    setStateCharacter: React.Dispatch<React.SetStateAction<Character>>;
+    stateEnemy: Character;
+    setStateEnemy: React.Dispatch<React.SetStateAction<Character>>;
+    p1Turno: boolean;
+    toggleTurno: ()=>void;
 }
 
 
-export default  function CardForP1({cardCharacter}:CardForGameProps) {
-    const energys = 18
-    const renderEnergy:string[] = Array.from({length:energys},()=>cardCharacter.energyEmoji)
-    const costAtackOne = Array.from({length:cardCharacter.atackOne.energyCost},()=>cardCharacter.energyEmoji).join(' ')
-    const costAtackTwo = Array.from({length:cardCharacter.atackTwo.energyCost},()=>cardCharacter.energyEmoji).join(' ')
+export default  function CardForP1({
+    stateCharacter, setStateCharacter,
+    stateEnemy, setStateEnemy,
+    p1Turno, toggleTurno
+}:CardForGameProps) {
+    const [energys, setEnergys] = useState(2)
+    const renderEnergy:string[] = Array.from({length:energys},()=>stateCharacter.energyEmoji)
+    const costAtackOne:string = Array.from({length:stateCharacter.atackOne.energyCost},()=>stateCharacter.energyEmoji).join(' ')
+    const costAtackTwo:string = Array.from({length:stateCharacter.atackTwo.energyCost},()=>stateCharacter.energyEmoji).join(' ')
+
+
+    function handleAtack(e: React.MouseEvent<HTMLButtonElement>){
+        const thisAtack = (e.target as HTMLButtonElement).name // isso me retorna "attackOne"
+        const thisPower:number = stateCharacter[thisAtack].power;
+        const thisEnergyCost:number = stateCharacter[thisAtack].energyCost;
+
+        if(energys>=thisEnergyCost){
+            setStateEnemy((prevState) => ({
+                ...prevState, // Copia todas as outras propriedades do estado anterior
+                life: prevState.life - thisPower, // Atualiza apenas o campo "life"
+            }));
+            setEnergys((prevEnergy)=>(prevEnergy-thisEnergyCost))
+            toggleTurno()
+            console.log(stateEnemy.life)
+        }else{
+            alert("A quantidade de energia não permite o atque")
+        }
+    }
+    function handleSkip(){
+        toggleTurno()
+        p1Turno?alert("Turno do player 2"):alert("Turno do player 1")
+        setEnergys(energys+1)
+    }
+
   return (
     <div className="flex">
         <div className="w-4/5 m-2 bg-slate-600 p-2">
             <div className="flex flex-row">
-                <div className="w-4/5 bg-cyan-200 text-center"><h1>{cardCharacter.name}</h1></div>
-                <div className="w-1/5 bg-cyan-200 text-center"><h3>{cardCharacter.life}</h3></div>
+                <div className="w-4/5 bg-cyan-200 text-center"><h1>{stateCharacter.name}</h1></div>
+                <div className="w-1/5 bg-cyan-200 text-center"><h3>{stateCharacter.life}</h3></div>
             </div>
             <div className="flex justify-center p-2 bg-slate-200 my-1">
-                <img src={cardCharacter.image} className="w-40 rounded"></img>
+                <img src={stateCharacter.image} className="w-40 rounded"></img>
             </div>
             <div className="flex bg-cyan-200 my-1">
                 <p className="w-2/6 my-auto">{costAtackOne}</p>
-                <button className="w-3/6 bg-slate-400 hover:bg-slate-600 my-4 mx-2 py-1 rounded">{cardCharacter.atackOne.name}</button>
-                <p className="w-1/6 my-auto">{cardCharacter.atackOne.power}</p>
+                <button name="atackOne" className="w-3/6 bg-slate-400 hover:bg-slate-600 my-4 mx-2 py-1 rounded" 
+                    onClick={(e)=>{handleAtack(e)}}>{stateCharacter.atackOne.name}
+                </button>
+                <p className="w-1/6 my-auto">{stateCharacter.atackOne.power}</p>
             </div>
             <div className="flex bg-cyan-200 my-1">
-            <p className="w-2/6 my-auto">{costAtackTwo}</p>
-                <button className="w-3/6 bg-slate-400 hover:bg-slate-600 my-4 mx-2 py-1 rounded">{cardCharacter.atackTwo.name}</button>
-                <p className="w-1/6 my-auto">{cardCharacter.atackTwo.power}</p>
+                <p className="w-2/6 my-auto">{costAtackTwo}</p>
+                <button name="atackTwo" className="w-3/6 bg-slate-400 hover:bg-slate-600 my-4 mx-2 py-1 rounded"
+                    onClick={(e)=>{handleAtack(e)}}>{stateCharacter.atackTwo.name}
+                </button>
+                <p className="w-1/6 my-auto">{stateCharacter.atackTwo.power}</p>
             </div>
             <div className="flex bg-cyan-200 my-1 justify-end">
-            <button className="bg-red-400 w-8 h-8 rounded m-1">
+            <button onClick={handleSkip} className="bg-red-400 w-8 h-8 rounded m-1">
                 <h1>🚫</h1>
             </button>
             </div>
